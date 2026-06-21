ebd757d1c2e6ddda9d9fe56bd2ce1714ec0acc74
nadya <nadya.vilboa@list.ru>
2026-06-21 20:16:31 +0300

    check login page

--- vite-project/src/views/Login.vue
+++ vite-project/src/views/Login.vue
@@ -1,5 +1,7 @@
 <script setup>
 import Header from './../components/Header.vue';
+import Graphics from '../components/Graphics.vue';
+import Icon from '../components/Icon.vue';
 </script>
 
 <template>
@@ -8,11 +10,18 @@ import Header from './../components/Header.vue';
       <h1 class="page-title visually-hidden">Eatly - Login</h1>
       <section class="banner">
         <div class="container">
-          <div class="banner__content">
-            <h2 class="banner__title">
+          <div class="banner__content banner__content--form form">
+            <h2 class="banner__title form__title">
               <span class="text-form">Sign In to eatly</span>
             </h2>
-            <div class="buttons-login"></div>
+            <div class="form__buttons-login">
+              <a class="form__btn-link" href="" target="_blank" aria-label="auth with google play">
+                <Icon iconName="google" iconWidth="22" iconHeight="23" fill="black" />
+              </a>
+              <a class="form__btn-link" href="" target="_blank" aria-label="auth with apple store">
+                <Icon iconName="apple" iconWidth="26" iconHeight="31" fill="black" />
+              </a>
+            </div>
             <p>or</p>
             <div class="form">
               <form action="" method="post">
@@ -32,69 +41,9 @@ import Header from './../components/Header.vue';
               <p>Create A New Account? <span>Sign Up</span></p>
             </div>
           </div>
-          <div class="banner__graphics graphics">
-              <div class="graphics__rest">
-                  <picture>
-                      <source type="image/webp" media="(min-width: 1280px)" srcset="./../assets/img/food-desktop-2.webp 1x, ./../assets/img/food-desktop-2-@2x.webp 2x">
-                      <source type="image/webp" media="(min-width: 1024px)" srcset="./../assets/img/food-tablet-2.webp 1x, ./../assets/img/food-tablet-2-@2x.webp 2x">
-                      <source type="image/webp" srcset="./../assets/img/food-mobile-2.webp 1x, ./../assets/img/food-mobile-2-@2x.webp 2x">
-                      <source media="(min-width:1280px)" srcset="./../assets/img/food-desktop-2.png 1x, ./../assets/img/food-desktop-2-@2x.png 2x"/> 
-                      <source media="(min-width:1024px)" srcset="./../assets/img/food-tablet-2.png 1x, ./../assets/img/food-tablet-2-@2x.png 2x"/>
-                      <img
-                          class="graphics__rest-img"
-                          src="./../assets/img/food-mobile-2.png"
-                          srcset="./../assets/img/food-mobile-2-@2x.png 2x"
-                          width="35"
-                          height="38"
-                          alt="dish vegetables and fish"
-                          loading="lazy"
-                      />
-                  </picture>
-                  <span class="graphics__rest-info">
-                      <span class="graphics__rest-name">Chicken Hell</span>
-                      <span class="graphics__rest-text">On The Way</span>
-                      <span class="graphics__rest-time">3:09 PM</span>
-                  </span>
-              </div>
-              <div class="graphics__ring-img-wrapper">
-                  <picture>
-                      <source type="image/webp" media="(min-width: 1280px)" srcset="./../assets/img/food-desktop-1.webp 1x, ./../assets/img/food-desktop-1-@2x.webp 2x">
-                      <source type="image/webp" media="(min-width: 1024px)" srcset="./../assets/img/food-tablet-1.webp 1x, ./../assets/img/food-tablet-1-@2x.webp 2x">
-                      <source type="image/webp" srcset="./../assets/img/food-mobile-1.webp 1x, ./../assets/img/food-mobile-1-@2x.webp 2x">
-                      <source media="(min-width:1280px)" srcset="./../assets/img/food-desktop-1.png 1x, ./../assets/img/food-desktop-1-@2x.png 2x"/> 
-                      <source media="(min-width:1024px)" srcset="./../assets/img/food-tablet-1.png 1x, ./../assets/img/food-tablet-1-@2x.png 2x"/>
-                      <img
-                          class="graphics__ring-img"
-                          src="./../assets/img/food-mobile-1.png"
-                          srcset="./../assets/img/food-mobile-1-@2x.png 2x"
-                          width="204"
-                          height="210"
-                          alt="dish vegetables and fish"
-                          loading="lazy"
-                      />
-                  </picture>
-              </div>
-              <div class="graphics__graph">
-                  <div class="graphics__graph-content">
-                      <picture>
-                          <source type="image/webp" media="(min-width: 1280px)" srcset="./../assets/img/graph-desktop.webp 1x, ./../assets/img/graph-desktop-@2x.webp 2x">
-                          <source type="image/webp" media="(min-width: 1024px)" srcset="./../assets/img/graph-tablet.webp 1x, ./../assets/img/graph-tablet-@2x.webp 2x">
-                          <source type="image/webp" srcset="./../assets/img/graph-mobile.webp 1x, ./../assets/img/graph-mobile-@2x.webp 2x">
-                          <source media="(min-width:1280px)" srcset="./../assets/img/graph-desktop.png 1x, ./../assets/img/graph-desktop-@2x.png 2x"/> 
-                          <source media="(min-width:1024px)" srcset="./../assets/img/graph-tablet.png 1x, ./../assets/img/graph-tablet-@2x.png 2x"/>
-                          <img
-                              src="./../assets/img/graph-mobile.png"
-                              srcset="./../assets/img/graph-mobile-@2x.png 2x"
-                              width="98"
-                              height="98"
-                              alt="graphic"
-                              loading="lazy"
-                          />
-                      </picture>
-                  </div>
-              </div>
-          </div>
+          <Graphics/>
         </div>
       </section>
     </main>
+    <div></div>
 </template>
--- vite-project/src/views/Main.vue
+++ vite-project/src/views/Main.vue
@@ -8,6 +8,7 @@ import DishesList from '../components/DishesList.vue';
 import Purchases from '../components/Purchases.vue';
 import Icon from '../components/Icon.vue';
 import Subscribe from '../components/Subscribe.vue';
+import Graphics from '../components/Graphics.vue';
 </script>
 
 <template>
@@ -49,68 +50,7 @@ import Subscribe from '../components/Subscribe.vue';
               </div>
               <Rating :value="5" :disabled="true" logo="trustpilot" :class="'banner__rating'" name="banner-rating" :ratingNumber="4900" />
           </div>
-          <div class="banner__graphics graphics">
-              <div class="graphics__rest">
-                  <picture>
-                      <source type="image/webp" media="(min-width: 1280px)" srcset="./../assets/img/food-desktop-2.webp 1x, ./../assets/img/food-desktop-2-@2x.webp 2x">
-                      <source type="image/webp" media="(min-width: 1024px)" srcset="./../assets/img/food-tablet-2.webp 1x, ./../assets/img/food-tablet-2-@2x.webp 2x">
-                      <source type="image/webp" srcset="./../assets/img/food-mobile-2.webp 1x, ./../assets/img/food-mobile-2-@2x.webp 2x">
-                      <source media="(min-width:1280px)" srcset="./../assets/img/food-desktop-2.png 1x, ./../assets/img/food-desktop-2-@2x.png 2x"/> 
-                      <source media="(min-width:1024px)" srcset="./../assets/img/food-tablet-2.png 1x, ./../assets/img/food-tablet-2-@2x.png 2x"/>
-                      <img
-                          class="graphics__rest-img"
-                          src="./../assets/img/food-mobile-2.png"
-                          srcset="./../assets/img/food-mobile-2-@2x.png 2x"
-                          width="35"
-                          height="38"
-                          alt="dish vegetables and fish"
-                          loading="lazy"
-                      />
-                  </picture>
-                  <span class="graphics__rest-info">
-                      <span class="graphics__rest-name">Chicken Hell</span>
-                      <span class="graphics__rest-text">On The Way</span>
-                      <span class="graphics__rest-time">3:09 PM</span>
-                  </span>
-              </div>
-              <div class="graphics__ring-img-wrapper">
-                  <picture>
-                      <source type="image/webp" media="(min-width: 1280px)" srcset="./../assets/img/food-desktop-1.webp 1x, ./../assets/img/food-desktop-1-@2x.webp 2x">
-                      <source type="image/webp" media="(min-width: 1024px)" srcset="./../assets/img/food-tablet-1.webp 1x, ./../assets/img/food-tablet-1-@2x.webp 2x">
-                      <source type="image/webp" srcset="./../assets/img/food-mobile-1.webp 1x, ./../assets/img/food-mobile-1-@2x.webp 2x">
-                      <source media="(min-width:1280px)" srcset="./../assets/img/food-desktop-1.png 1x, ./../assets/img/food-desktop-1-@2x.png 2x"/> 
-                      <source media="(min-width:1024px)" srcset="./../assets/img/food-tablet-1.png 1x, ./../assets/img/food-tablet-1-@2x.png 2x"/>
-                      <img
-                          class="graphics__ring-img"
-                          src="./../assets/img/food-mobile-1.png"
-                          srcset="./../assets/img/food-mobile-1-@2x.png 2x"
-                          width="204"
-                          height="210"
-                          alt="dish vegetables and fish"
-                          loading="lazy"
-                      />
-                  </picture>
-              </div>
-              <div class="graphics__graph">
-                  <div class="graphics__graph-content">
-                      <picture>
-                          <source type="image/webp" media="(min-width: 1280px)" srcset="./../assets/img/graph-desktop.webp 1x, ./../assets/img/graph-desktop-@2x.webp 2x">
-                          <source type="image/webp" media="(min-width: 1024px)" srcset="./../assets/img/graph-tablet.webp 1x, ./../assets/img/graph-tablet-@2x.webp 2x">
-                          <source type="image/webp" srcset="./../assets/img/graph-mobile.webp 1x, ./../assets/img/graph-mobile-@2x.webp 2x">
-                          <source media="(min-width:1280px)" srcset="./../assets/img/graph-desktop.png 1x, ./../assets/img/graph-desktop-@2x.png 2x"/> 
-                          <source media="(min-width:1024px)" srcset="./../assets/img/graph-tablet.png 1x, ./../assets/img/graph-tablet-@2x.png 2x"/>
-                          <img
-                              src="./../assets/img/graph-mobile.png"
-                              srcset="./../assets/img/graph-mobile-@2x.png 2x"
-                              width="98"
-                              height="98"
-                              alt="graphic"
-                              loading="lazy"
-                          />
-                      </picture>
-                  </div>
-              </div>
-          </div>
+          <Graphics/>
       </div>
     </section>
     <DataCompany amountCustomers="10K+" amountDishes="4M" levelSupport="99.99%" />
